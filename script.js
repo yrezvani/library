af899d95df039d32e68bf0f492ae14367afb6447
SharpLad <143443228+yrezvani@users.noreply.github.com>
2023-10-24 15:23:22 +0100

    Add JS logic for book already in library

--- script.js
+++ script.js
@@ -12,14 +12,6 @@ const Book = function (title, author, pages, read) {
 
 const myLibrary = [];
 
-const addBook = function () {
-    const title = prompt ('Enter the title of the book');
-    const author = prompt('Enter the name of the author');
-    const pages = prompt('Enter the number of pages');
-    const read = prompt('Have you read the book? Enter true or false');
-    myLibrary.push(new Book(title, author, pages, read));
-}
-
 myLibrary.push(new Book('Shahnama', 'Ferdowsi', 300, true));
 myLibrary.push(new Book('Iliad', 'Homer', 400, false));
 myLibrary.push(new Book('McBeth', 'Shakespear', 500, false));
@@ -48,34 +40,49 @@ form.addEventListener('submit', function(e) {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
-    const readVal = document.querySelector('#read').checked;
+    const read = document.querySelector('#read').checked;
+    const titles = [];
 
-    myLibrary.push(new Book(title, author, pages, readVal));
-    // New card
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('cards');
-    cardsWrapper.appendChild(newDiv);
     
-    // Card contents
-    const titleEl = document.createElement('h3');
-    titleEl.textContent = title;
-    const authorEl = document.createElement('h3');
-    authorEl.textContent = author;
-    const pagesEl = document.createElement('h3');
-    pagesEl.textContent = pages
-    const readBtn = document.createElement('button');
-    readBtn.textContent = readVal === true ? 'Read' : 'Not read';
-    console.log(readVal);
-    readVal === true ? readBtn.classList.add('read') : readBtn.classList.add('unread');
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = 'Remove';
-    removeBtn.classList.add('remove');
-    newDiv.append(titleEl, authorEl, pagesEl, readBtn, removeBtn);
+    for (const book of myLibrary) {
+        titles.push(book.title);
+    }
 
-});
+
+    if (titles.includes(title)) {
+        alert('This book is already in the library')
+    } else {
+        myLibrary.push(new Book(title, author, pages, read));
+        // New card
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('new-cards');
+        cardsWrapper.appendChild(newDiv);
+
+
+        // Card contents
+        const titleEl = document.createElement('h3');
+        titleEl.textContent = title;
+        const authorEl = document.createElement('h3');
+        authorEl.textContent = author;
+        const pagesEl = document.createElement('h3');
+        pagesEl.textContent = pages
+        const readBtn = document.createElement('button');
+        readBtn.textContent = read === true ? 'Read' : 'Not read';
+        read === true ? readBtn.classList.add('read') : readBtn.classList.add('unread');
+        const removeBtn = document.createElement('button');
+        removeBtn.textContent = 'Remove';
+        removeBtn.classList.add('remove');
+        newDiv.append(titleEl, authorEl, pagesEl, readBtn, removeBtn);
+        // console.log(myLibrary.findIndex((x) => x.title === title));
+    };
+})
 
 // Remove books (not working)
+const newDiv = document.querySelector('.new-cards');
+const removeBtn = document.querySelector('.remove');
 
-removeBtn.addEventListener('click', function() {
-    this.newDiv.remove();
-})
+// removeBtn.addEventListener('click', function() {
+
+//     this.newDiv.remove();
+//     console.log('working');
+// })
