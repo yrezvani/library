69ecccff03a7dedf754d9c0644698d5bde62816a
SharpLad <143443228+yrezvani@users.noreply.github.com>
2023-10-27 09:37:38 +0100

    Update JS to get remove button working update remove button to remove both card and book remove placeholder cards from HTML remove unnecessary JS lines

--- script.js
+++ script.js
@@ -34,21 +34,33 @@ addButton.addEventListener('click', function() {
 })
 
 // Add books
-form.addEventListener('submit', function(e) {
+// remove button event listener function
+document.addEventListener('click', (e) => {
+    const element = e.target.closest('.remove');
+    if (element) {
+        myLibrary.splice(e.target.dataset.btn, 1);
+        titles.splice(e.target.dataset.btn);
+        e.target.closest('.new-cards').remove();
+    }
+})
+
+
+// Initialize titles array
+const titles = []
+for (const book of myLibrary) {
+    titles.push(book.title);
+}
+
+// Create book function
+const createCard = function (e) {
     e.preventDefault();
     dialog.close();
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
     const read = document.querySelector('#read').checked;
-    const titles = [];
-
     
-    for (const book of myLibrary) {
-        titles.push(book.title);
-    }
-
-
+    // Check if book is already in library
     if (titles.includes(title)) {
         alert('This book is already in the library')
     } else {
@@ -56,33 +68,31 @@ form.addEventListener('submit', function(e) {
         // New card
         const newDiv = document.createElement('div');
         newDiv.classList.add('new-cards');
+        newDiv.dataset.div = myLibrary.findIndex((x) => x.title === title);
         cardsWrapper.appendChild(newDiv);
 
 
-        // Card contents
-        const titleEl = document.createElement('h3');
-        titleEl.textContent = title;
-        const authorEl = document.createElement('h3');
-        authorEl.textContent = author;
-        const pagesEl = document.createElement('h3');
-        pagesEl.textContent = pages
-        const readBtn = document.createElement('button');
-        readBtn.textContent = read === true ? 'Read' : 'Not read';
-        read === true ? readBtn.classList.add('read') : readBtn.classList.add('unread');
-        const removeBtn = document.createElement('button');
-        removeBtn.textContent = 'Remove';
-        removeBtn.classList.add('remove');
-        newDiv.append(titleEl, authorEl, pagesEl, readBtn, removeBtn);
-        // console.log(myLibrary.findIndex((x) => x.title === title));
-    };
+    // Card contents
+    const titleEl = document.createElement('h3');
+    titleEl.textContent = title;
+    const authorEl = document.createElement('h3');
+    authorEl.textContent = author;
+    const pagesEl = document.createElement('h3');
+    pagesEl.textContent = pages
+    const readBtn = document.createElement('button');
+    readBtn.textContent = read === true ? 'Read' : 'Not read';
+    read === true ? readBtn.classList.add('read') : readBtn.classList.add('unread');
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.classList.add('remove');
+    removeBtn.dataset.btn = myLibrary.findIndex((x) => x.title === title);
+    newDiv.append(titleEl, authorEl, pagesEl, readBtn, removeBtn);
+    titles.push(title);
+    }
+}
+
+
+form.addEventListener('submit', function(e) {
+    createCard(e);
 })
 
-// Remove books (not working)
-const newDiv = document.querySelector('.new-cards');
-const removeBtn = document.querySelector('.remove');
-
-// removeBtn.addEventListener('click', function() {
-
-//     this.newDiv.remove();
-//     console.log('working');
-// })
